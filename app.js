bec640ade6381543b203daa4a3c358e5fc9e3cfb
Abul Kibria <ahkibria@hotmail.co.uk>
2023-06-21 01:18:40 +0100

    my first commit for TypeScript after installation process

---
+++ app.js
@@ -0,0 +1,4 @@
+var message = "I am learning typescript and its fun";
+var message1 = " Its very complicated as well";
+console.log(message);
+console.log(message1);
---
+++ app.ts
@@ -0,0 +1,4 @@
+let message: string = "I am learning typescript and its fun"
+let message1 : string = " Its very complicated as well"
+console.log(message)
+console.log(message1)
